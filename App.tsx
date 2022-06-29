809e339011bac0d9f393fb1cae6b0c9ad04f332c
Pablo Henrique <pablohdev@outlook.com>
2022-06-29 16:08:52 -0300

    estrutura inicial

--- App.tsx
+++ App.tsx
@@ -1,20 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import App from './src'
 
-export default function App() {
+export default function Main() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <App />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {View} from 'react-native';
+import { StatusBar } from "expo-status-bar";
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <StatusBar style="light" />
+    </View>
+  );
+};
+
+export default App;
